34359e3760ad77d16aee49ae05351b9d867efc4f
Caíque Souto Lima <caiquesouto@hotmail.com>
2021-11-25 14:29:15 -0300

    Payable class added to model method

--- src/models/Payables.ts
+++ src/models/Payables.ts
@@ -9,10 +9,18 @@ export class Payable {
     constructor(
         private value: number,
         private status: string,
-        private paymentDate: string,
+        private paymentDate: string
     ) { }
 
     public getValue = (): number => this.value
     public getStatus = (): string => this.status
     public getPaymentDate = (): string => this.paymentDate
+
+    public static toPayableModel(payable: PayableData): Payable {
+        return new Payable(
+            payable.value, 
+            payable.status,
+            payable.paymentDate
+        )
+    }
 }
