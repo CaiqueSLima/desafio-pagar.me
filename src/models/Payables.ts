ee7c2cbf24fc94ee28cc3c691156740aa74a5100
Caíque Souto Lima <caiquesouto@hotmail.com>
2021-11-25 14:26:33 -0300

    Payable class completed

--- src/models/Payables.ts
+++ src/models/Payables.ts
@@ -1,5 +1,18 @@
+export interface PayableData {
+    value: number
+    status: string
+    paymentDate: string
+}
+
 export class Payable {
+
     constructor(
-        
+        private value: number,
+        private status: string,
+        private paymentDate: string,
     ) { }
+
+    public getValue = (): number => this.value
+    public getStatus = (): string => this.status
+    public getPaymentDate = (): string => this.paymentDate
 }
